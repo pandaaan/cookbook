92b6a2b303179c0cd7b9601c0cc0d2e8edfc4034
pandaaan <nhudan06@gmail.com>
2020-01-23 13:23:06 +0100

    Updates

---
+++ precache-manifest.60253ef779b1e4da87cb25b68ce774cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3acc99371c0ff44de9be82d9f219ff9b",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "c9f38ee04ccff3903816",
+    "url": "/react-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d91123d946cdef246c9b",
+    "url": "/react-gh-pages/static/js/2.dd819db5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-gh-pages/static/js/2.dd819db5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c9f38ee04ccff3903816",
+    "url": "/react-gh-pages/static/js/main.2af58196.chunk.js"
+  },
+  {
+    "revision": "64459accd745e0285d97",
+    "url": "/react-gh-pages/static/js/runtime-main.8f89723c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
